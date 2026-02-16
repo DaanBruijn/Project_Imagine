588c01b95b666399c65baba5ac39632a8301f0ef
Daniël Bruijn <bruijndaniel1308@gmail.com>
2026-02-16 12:43:32 +0100

    Bus Scene Added
    Added a scene where the bus drives away from the player.
    
    Scene Has:
    -Fog Enviroment
    -Bus stop (ish) setting
    -Skyscrappers
    -New Materials and textures added
    
    Adjusted
    -Moving Bus Script
    -Some of the Folders not being correct in project file

--- Assets/Script/ConceptVideo/vroomvroom.cs
+++ Assets/Script/ConceptVideo/vroomvroom.cs
@@ -4,16 +4,27 @@ public class vroomvroom : MonoBehaviour
 {
     [SerializeField] private float _vroomSpeed;
     private Rigidbody _vroom;
+    private bool _canBusMove;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _vroom = GetComponent<Rigidbody>();
+        _canBusMove = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _vroom.AddForce(Vector3.forward * _vroomSpeed,ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space))
+            _canBusMove = true;
+        
+        MoveBus();
+    }
+
+    void MoveBus()
+    {
+        if (_canBusMove)
+            _vroom.AddForce(Vector3.left * _vroomSpeed,ForceMode.Impulse);
     }
 }
