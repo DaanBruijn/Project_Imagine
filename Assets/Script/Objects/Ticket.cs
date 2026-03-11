d98212c5dff023cf64f55bf09b1da599959cccea
Syntax-0-0 <waaldercc@gmail.com>
2026-03-11 14:56:16 +0100

    SceneMerge
    MergedPlaytestScenes

--- Assets/Script/Objects/Ticket.cs
+++ Assets/Script/Objects/Ticket.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 public class Ticket : BaseObjectScript
 {
     public bool isEndTicket = false;
-
+    public static bool holdingTicket;
     public override void ActivateObject()
     {
-        transform.position = Camera.main.transform.position + (Camera.main.transform.forward * 1) + (Camera.main.transform.right * 1);
-        transform.parent = Camera.main.transform;
+        if (!holdingTicket)
+        {
+            holdingTicket = true;
+            transform.position = Camera.main.transform.position + (Camera.main.transform.forward * 1) + (Camera.main.transform.right * 1);
+            transform.parent = Camera.main.transform;
+        }
     }
     public void UseTicket()
     {
-
+        holdingTicket = false;
         Destroy(this.gameObject);
     }
 }
