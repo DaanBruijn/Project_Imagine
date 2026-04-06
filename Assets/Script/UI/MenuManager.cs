085a7785deef9601e5bacc316357e956627aa444
Daniël Bruijn <bruijndaniel1308@gmail.com>
2026-04-06 18:11:31 +0200

    MainMenu Scene added
    Scene Added for the MainMenu WHOOP WHOOP
    
    -Start and Exit working with a fade in
    
    -Buttons make funny sounds

--- Assets/Script/UI/MenuManager.cs
+++ Assets/Script/UI/MenuManager.cs
@@ -1,15 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// - MenuManager script for buttons
+// - Daniel Bruijn & Cesar Waalders
 
 public class MenuManager : MonoBehaviour
 {
+    // - Variables
+    [Header("Audio")]
+    public List<AudioClip> audioClips = new List<AudioClip>();
+    public AudioSource audioSource;
+    
+    [Header("Image")]
+    public Image fadeImage;
+    public float fadeDuration = 1f;
+    
     public void LoadScene(string sceneName)
     {
+        Debug.Log("Loading scene: " + sceneName);
+        StartCoroutine(FadeAndLoad(sceneName));
+    }
+
+    IEnumerator FadeAndLoad(string sceneName)
+    {
+        float time = 0f;
+        Color color = fadeImage.color;
+
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            color.a = Mathf.Lerp(0, 1, time / fadeDuration);
+            fadeImage.color = color;
+            yield return null;
+        }
+
         SceneManager.LoadScene(sceneName);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ButtonSound()
+    {
+        audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Count)]);
     }
 }
